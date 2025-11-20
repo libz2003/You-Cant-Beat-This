e6a71a6bf262cb9a92dc9a24cd557f1844cd0c2d
libz2003 <libz2003@outlook.com>
2025-11-19 23:37:03 -0500

    add exploit: place on path

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -11,6 +11,7 @@ public class Turret : MonoBehaviour
     public float range = 150.0f;
     public float fireRate = 1.0f;
     public float rotationSpeed = 10.0f;
+    public float tooCloseRange = 30.0f;
 
     [Header("Unity Setup Fields")]
 
@@ -47,7 +48,15 @@ public class Turret : MonoBehaviour
 
         if (nearestEnemy != null && shortestDistance <= range)
         {
-            target = nearestEnemy.transform;
+            // exploit: if too close then enemy is destroyed
+            if (shortestDistance <= tooCloseRange)
+            {
+                Destroy(nearestEnemy);
+            }
+            else
+            {
+                target = nearestEnemy.transform;
+            }
         }
         else
         {
