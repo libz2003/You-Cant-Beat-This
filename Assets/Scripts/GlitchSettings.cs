5f0c7eae0d974b3be1b03e2d3855e0b1c1566c4f
Juan Pablo Amezcua Gonzalez <jpabloamezcua@HWQ79FQ1CM.tld>
2025-11-22 19:13:40 -0500

    bug settings

--- Assets/Scripts/GlitchSettings.cs
+++ Assets/Scripts/GlitchSettings.cs
@@ -2,22 +2,19 @@ using UnityEngine;
 
 public class GlitchSettings : MonoBehaviour
 {
-    public GameObject visualMenu;
-
-    public GameObject invisibleWall;
+    public GameObject settingsPanel;
 
     private bool isSettingsOpen = false;
 
     void Start()
     {
-        if(visualMenu != null) visualMenu.SetActive(false);
-        if(invisibleWall != null) invisibleWall.SetActive(false);
+        if(settingsPanel != null)
+            settingsPanel.SetActive(false);
     }
-
     public void ToggleSettings()
     {
         isSettingsOpen = !isSettingsOpen;
-        if(visualMenu != null) visualMenu.SetActive(isSettingsOpen);
-        if(invisibleWall != null) invisibleWall.SetActive(isSettingsOpen);
+        if(settingsPanel != null)
+            settingsPanel.SetActive(isSettingsOpen);
     }
 }
