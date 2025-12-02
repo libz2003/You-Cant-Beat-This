24f235725241a32f2b9a339937e9b0c15f693f05
libz2003 <libz2003@outlook.com>
2025-12-02 03:59:05 -0500

    tree bug

---
+++ Assets/Scripts/Tree/TreeCollisionManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class TreeCollisionManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (!PersistentSettings.instance.treeCuttable)
+        {
+            gameObject.GetComponent<Collider>().enabled = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision detected");
+
+        if (collision.gameObject.CompareTag("Bullet") || collision.gameObject.CompareTag("GunBullet"))
+        {
+            // TODO: audio
+        }
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            PersistentSettings.instance.targetTreeCuttable = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Tree/TreeTipCollisionManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class TreeTipCollisionManager : MonoBehaviour
+{
+    void Start()
+    {
+        if (!PersistentSettings.instance.treeCuttable)
+        {
+            gameObject.GetComponent<Collider>().enabled = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Tree Tip Collision detected");
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            PersistentSettings.instance.targetTreeCuttable = false;
+        }
+    }
+}
