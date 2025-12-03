b0283dc250a276fa5bbde605b4bf5445c65f564e
oliver <122304380+oolliivveerr@users.noreply.github.com>
2025-12-03 02:29:17 -0500

    lose screen

--- Assets/Scripts/UIs/PlayAgain.cs
+++ Assets/Scripts/UIs/PlayAgain.cs
@@ -1,9 +1,19 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayAgain : MonoBehaviour
 {
     public void Restart()
     {
         Universe.instance.Restart();
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Restart();
+        }
     }
 }
