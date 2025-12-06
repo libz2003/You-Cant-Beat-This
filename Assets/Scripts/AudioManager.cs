d80bd9ea1872d32d4dfeaeb784dd18416bc970b0
libz2003 <libz2003@outlook.com>
2025-12-05 22:55:34 -0500

    dont destroy audiomanager ;

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -26,6 +26,7 @@ public class AudioManager : MonoBehaviour
 
     void Awake()
     {
+        DontDestroyOnLoad(gameObject);
         if (Instance == null)
             Instance = this;
         else
